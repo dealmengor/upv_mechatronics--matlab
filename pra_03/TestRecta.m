1ab2a7a34ea5e5292fa1c490b7965cd6887ba5e3
Alexander Almengor <my.adresse@hotmail.com>
2021-04-03 17:45:19 +0200

    practice 3 backup

--- pra_03/TestRecta.m
+++ pra_03/TestRecta.m
@@ -1,8 +1,10 @@
 %Punto C
-function TestRecta
+function TestRecta(X, Y, corte_inicio)
     %Vectores
-    X = [0.0 0.5 1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0 5.5 6.0];
-    Y = [12.0 9.5 7.0 5.2 4.0 3.1 2.7 2.0 1.5 1.0 0.5 0.2 0.1];
+    %X = [0.0 0.5 1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0 5.5 6.0];
+    %Y = [12.0 9.5 7.0 5.2 4.0 3.1 2.7 2.0 1.5 1.0 0.5 0.2 0.1];
+    X = double(X);
+    Y = double(Y);
 
     plot(X, Y, '.', 'MarkerSize', 10)
     ylabel('Valor medido')
@@ -12,7 +14,7 @@ function TestRecta
 
     %Puntos Extremos
     [m1, b1] = RectaPuntosExtremos(X, Y);
-    y_extremos = m1 * X + b1;
+    y_extremos = m1 * (X - corte_inicio) + b1;
     plot(X, y_extremos)
 
     %Mínimos Cuadrados
---
+++ pra_03/testSonar_distancia.m
@@ -0,0 +1,39 @@
+global distancia_v;
+global tiempo_v;
+tiempo = 0;
+Ts = 0.05;
+SetSensorUltrasonic(IN_1); % informamos que el sensor ultrasonico esta en el puerto 1
+
+TextOut(0,LCD_LINE1,'test Sonar'); % Mostramos por pantalla el tipo de test
+TextOut(0,LCD_LINE2,'Presione el boton central para');
+TextOut(0,LCD_LINE3,'comenzar con el test');
+while(~ButtonPressed(BTNCENTER))end % Esperamos a pulsar el boton intro
+ClearScreen(); 
+
+OnFwd(OUT_AC,40);
+
+%TEST SENSOR ULTRASONICO (MEDIDAS)
+distancia = SensorUS(IN_1);
+mensaje = ['distancia = ' num2str(distancia)];
+TextOut(0,LCD_LINE1,mensaje);
+while distancia > 20 % hasta que la distancia sea menor que 20 cm
+    t1=double(CurrentTick());
+    distancia = SensorUS(IN_1);
+    tiempo_v = [tiempo_v tiempo];
+    distancia_v = [distancia_v distancia];
+    mensaje = ['distancia = ' num2str(distancia)]; % mostramos la distancia por pantalla
+    TextOut(0,LCD_LINE1,mensaje);
+    
+    t2=double(CurrentTick());
+    % Espera hasta el siguiente periodo
+    espera=(max(0,Ts*1000-(t2-t1)));
+    Wait(espera);
+    tiempo = tiempo + Ts; 
+end
+Off(OUT_AC); % paramos los motores
+Wait(2000);
+
+TextOut(0,LCD_LINE1,'Presione el boton central para'); % pulsamos el botón central para finalizar el programa
+TextOut(0,LCD_LINE2,'finalizar el test');
+while(~ButtonPressed(BTNCENTER))end
+Stop(0);
